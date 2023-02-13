6efaec27525f2812d9fc02dc55cc429f1fe89714
olers <1079750500@qq.com>
2023-02-13 17:33:31 +0800

    实现:BR, JMP, JSR, LD, LDI

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 
 /// @brief register
-enum { R_R0 = 0,
-       R_R1,
-       R_R2,
-       R_R3,
-       R_R4,
-       R_R5,
-       R_R6,
-       R_R7,
-       R_PC,
-       R_COND,
-       R_COUNT };
+enum Register {
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT
+};
 
 /// @brief options
-enum {
+enum Options {
     OP_ADD = 0, /* add  */
     OP_AND,     /* bitwise and */
     OP_BR,      /* branch */
@@ -33,11 +35,11 @@ enum {
     OP_TRAP     /* execute trap */
 };
 
-/// @brief R_COND - 存储「条件标志位」
-enum {
-    FL_POS = 1 << 0, /* P */
-    FL_ZRO = 1 << 1, /* ZRO */
-    FL_NEG = 1 << 2, /* NEG */
+/// @brief R_COND Condition - 存储「条件标志位」
+enum Condition {
+    FL_POS = 1 << 0, /* Positive */
+    FL_ZRO = 1 << 1, /* Zero */
+    FL_NEG = 1 << 2, /* Negative */
 };
 
 uint16_t memory[UINT16_MAX];
@@ -52,6 +54,7 @@ uint16_t sign_extend(uint16_t x, int bit_count) {
     return x;
 }
 
+/// @brief setcc()
 void update_flags(uint16_t r) {
     if (reg[r] == 0) {
         reg[R_COND] = FL_ZRO;
@@ -62,8 +65,11 @@ void update_flags(uint16_t r) {
     }
 }
 
-uint16_t mem_read(uint16_t r) {
+uint16_t mem_read(uint16_t address) {
     return 0;
+}
+
+void mem_write(uint16_t address, uint16_t val) {
 }
 
 int main(int argc, const char *argv[]) {
@@ -91,9 +97,9 @@ int main(int argc, const char *argv[]) {
         uint16_t op = instr >> 12;
         switch (op) {
         case OP_ADD: {
-            uint16_t dr = (instr >> 9) & 0x7;
-            uint16_t sr1 = (instr >> 6) & 0x7;
-            uint16_t imm_flag = (instr >> 5) & 0x1;
+            uint16_t dr = instr >> 9 & 0x7;
+            uint16_t sr1 = instr >> 6 & 0x7;
+            uint16_t imm_flag = instr >> 5 & 0x1;
             if (imm_flag) {
                 uint16_t imm5 = instr & 0x1F;
                 reg[dr] = reg[sr1] + sign_extend(imm5, 5);
@@ -105,9 +111,9 @@ int main(int argc, const char *argv[]) {
             break;
         }
         case OP_AND: {
-            uint16_t dr = (instr >> 9) & 0x7;
-            uint16_t sr1 = (instr >> 6) & 0x7;
-            uint16_t imm_flag = (instr >> 5) & 0x1;
+            uint16_t dr = instr >> 9 & 0x7;
+            uint16_t sr1 = instr >> 6 & 0x7;
+            uint16_t imm_flag = instr >> 5 & 0x1;
             if (imm_flag) {
                 uint16_t imm5 = instr & 0x1F;
                 reg[dr] = reg[sr1] & sign_extend(imm5, 5);
@@ -119,19 +125,44 @@ int main(int argc, const char *argv[]) {
             break;
         }
         case OP_BR: {
-
+            // uint16_t n_flag = (instr >> 11) & 0x1;
+            // uint16_t z_flag = (instr >> 10) & 0x1;
+            // uint16_t p_flag = (instr >> 9) & 0x1;
+            uint16_t nzp_flag = instr >> 9 & 0x7;
+            uint16_t pc_offset = instr & 0x1FF;
+            if (nzp_flag & reg[R_COND]) {
+                reg[R_PC] += sign_extend(pc_offset, 9);
+            }
             break;
         }
         case OP_JMP: {
+            uint16_t base_r = instr >> 6 & 0x7;
+            reg[R_PC] = reg[base_r];
             break;
         }
         case OP_JSR: {
+            reg[R_R7] = reg[R_PC];
+            if ((instr >> 11 & 1) == 0) {
+                uint16_t base_r = instr >> 6 & 0x7;
+                reg[R_PC] = reg[base_r];
+            } else {
+                uint16_t pc_offset_11 = instr & 0x7FF;
+                reg[R_PC] += sign_extend(pc_offset_11, 11);
+            }
             break;
         }
         case OP_LD: {
+            uint16_t dr = instr >> 9 & 0x7;
+            uint16_t pc_offset_9 = instr & 0x1FF;
+            mem_write(reg[R_PC] + sign_extend(pc_offset_9, 9), reg[dr]);
+            update_flags(dr);
             break;
         }
         case OP_LDI: {
+            uint16_t dr = instr >> 9 & 0x7;
+            uint16_t pc_offset_9 = instr & 0x1FF;
+            mem_write(mem_read(reg[R_PC] + sign_extend(pc_offset_9, 9)), reg[dr]);
+            update_flags(dr);
             break;
         }
         case OP_LDR: {
